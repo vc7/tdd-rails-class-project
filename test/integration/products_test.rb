fd6da7dcd72329523445cafe1cb7571c5e2a0b9f
Vincent Chen <vince78718@gmail.com>
2015-07-08 21:07:14 +0800

    定義 setup method ，在裡面取得工廠女孩裡面的假資料 - setup method 會在每一次的 test 執行前都會執行一次

--- test/integration/products_test.rb
+++ test/integration/products_test.rb
@@ -1,15 +1,16 @@
 require 'test_helper'
 
 class ProductsTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@book = create(:php_book)
+	end
+
 	test "can add product successfully" do
-		# 現在直接從工廠女孩取資料
-		# create 因為有在小幫手載入，所以不用從 FG 取
-		# （待確認）放在 Product.count 會造成多算一個，所以拉到前面來
-		book = create(:php_book)
 
 		count1 = Product.count
 
-		post '/products', product: {name:book.name, price:book.price}
+		post '/products', product: {name:@book.name, price:@book.price}
 		assert_redirected_to '/products' # 期望會轉址到 /products
 		assert_equal count1+1, Product.count # 期望做完某些事情之後，總數等於原本的 + 1
 	end
