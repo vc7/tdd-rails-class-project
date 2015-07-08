056b558720f3f851a61fbe3347e205ab3033abb9
Vincent Chen <vince78718@gmail.com>
2015-07-08 20:35:42 +0800

    新增測試，如果有漏欄位，就不能寫入

--- test/integration/products_test.rb
+++ test/integration/products_test.rb
@@ -7,4 +7,12 @@ class ProductsTest < ActionDispatch::IntegrationTest
 		assert_redirected_to '/products' # 期望會轉址到 /products
 		assert_equal count1+1, Product.count # 期望做完某些事情之後，總數等於原本的 + 1
 	end
+
+	# 期望欄位不足，不會成功寫入
+	test "can not add product successfully" do
+		count1 = Product.count
+		post '/products', product: {name:'', price:100}
+		assert_redirected_to '/products' 
+		assert_equal count1, Product.count 
+	end
 end
