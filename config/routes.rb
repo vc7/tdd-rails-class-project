8c313d6cf49f2e14083c5fc869492db5dc0301ad
Vincent Chen <vince78718@gmail.com>
2015-07-08 19:49:08 +0800

    新增一個 help 的 route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
 
   get '/contact', to: 'static_pages#contact' # 拿到 /contact 之後，幫我倒到 static_pages controller 的 contact 方法
+  get '/help', to: 'static_pages#help'
 end
