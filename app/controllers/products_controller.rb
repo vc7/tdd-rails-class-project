7913877e4627de8498d11ff2b3e268a1023ca1e1
Vincent Chen <vince78718@gmail.com>
2015-07-08 20:27:21 +0800

    測試失敗，嘗試取得資料並寫入資料庫，這時候因為沒有設定強參數，所以無法存入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
 	end
 
 	def create
+		product = Product.new(params[:product])
+
+		# 呼叫 save 方法 就會存進去
+		product.save
+
 		# 不管如何，先導到 index 去，路徑是 products_path
 		redirect_to products_path
 	end
