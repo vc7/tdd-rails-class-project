57497c0c5ad074c5104adf721883ff246c0725c7
Vincent Chen <vince78718@gmail.com>
2015-07-08 19:29:35 +0800

    撰寫第一個測試

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+	
+	# 執行階段，會被轉換成 def test_should_get_contact_page
+	test "should get contact page" do
+		get :contact
+		assert_response :success
+	end
 end
