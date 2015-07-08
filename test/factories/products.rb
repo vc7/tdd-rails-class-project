56fc767b22ea4684593fdda03ebb4cd2e59c6624
Vincent Chen <vince78718@gmail.com>
2015-07-08 21:22:57 +0800

    在工廠女孩的假資料使用 Faker

--- test/factories/products.rb
+++ test/factories/products.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   # class 沒寫的話，會根據檔名猜是什麼 class
   factory :ruby_book, class: Product do
-    name "Ruby Book"
+    name Faker::Name.name
     price 100
   end
 
   factory :php_book, class: Product do
-    name "PHP Book"
+    name Faker::Name.name
     price 50
   end
 end
