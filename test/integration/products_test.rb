55a55fb9d741f6e4af97eb3f11846fd96e334228
Vincent Chen <vince78718@gmail.com>
2015-07-08 20:44:36 +0800

    測試的內容改從 fixtures 中載入

--- test/integration/products_test.rb
+++ test/integration/products_test.rb
@@ -3,7 +3,12 @@ require 'test_helper'
 class ProductsTest < ActionDispatch::IntegrationTest
 	test "can add product successfully" do
 		count1 = Product.count
-		post '/products', product: {name:'ruby', price:100}
+
+		# 從 fixture/products.yml 裡面取得 :php 物件
+		# test 會去找同名的 yml 檔案
+		book = products(:php)
+
+		post '/products', product: {name:book.name, price:book.price}
 		assert_redirected_to '/products' # 期望會轉址到 /products
 		assert_equal count1+1, Product.count # 期望做完某些事情之後，總數等於原本的 + 1
 	end
