20a1886862fe2fdb25577aa6db8dc08fe20e30a3
Vincent Chen <vince78718@gmail.com>
2015-07-08 20:15:43 +0800

    測試失敗，新增 Product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708111517) do
+ActiveRecord::Schema.define(version: 20150708121501) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
